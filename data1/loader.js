4d01a8fe52cc48b77931e07a8bff921ed5fd8847
BurritoEdition <100502707+BurritoEdition@users.noreply.github.com>
2026-01-08 20:39:10 -0500

    Refactor loader.js for improved readability and structure

--- data1/loader.js
+++ data1/loader.js
@@ -1,72 +1,106 @@
+EJS_AdUrl = 'https://burritoedition.com/ad/';
 
-EJS_AdUrl = 'https://burritoedition.com/ad/index.html'
-
-
-var VERSION = '0.4.26'
+var VERSION = '0.4.26';
 fetch('https://raw.githack.com/ethanaobrien/emulatorjs/main/data/version.json').then(response => {
-	if (response.ok) {
-		response.text().then(body => {
-			var version = JSON.parse(body);
-			var usingVersion = VERSION;
-			if (usingVersion != version.current_version) {
-				console.log('█▄▄ █░█ █▀█ █▀█ █ ▀█▀ █▀█   █▀▀ █▀▄ █ ▀█▀ █ █▀█ █▄░█\n█▄█ █▄█ █▀▄ █▀▄ █ ░█░ █▄█   ██▄ █▄▀ █ ░█░ █ █▄█ █░▀█');
-			};
-		});
-	};
+  if (response.ok) {
+    response.text().then(body => {
+      var version = JSON.parse(body);
+      var usingVersion = VERSION;
+      if (usingVersion != version.current_version) {
+        console.log('█▄▄ █░█ █▀█ █▀█ █ ▀█▀ █▀█   █▀▀ █▀▄ █ ▀█▀ █ █▀█ █▄░█\n█▄█ █▄█ █▀▄ █▀▄ █ ░█░ █▄█   ██▄ █▄▀ █ ░█░ █ █▄█ █░▀█');
+      }
+    });
+  }
 });
-var scriptTag = document.getElementsByTagName('script')[0]
-var emu_main = document.createElement('script')
-emu_main.src = function() {
-    if ('undefined' != typeof EJS_paths && typeof EJS_paths['emu-main.js'] == 'string') {
-        return EJS_paths['emu-main.js']
-    } else if ('undefined' != typeof EJS_pathtodata) {
-        return EJS_pathtodata + 'emu-main.js?v=' + VERSION
+
+var scriptTag = document.getElementsByTagName('script')[0];
+var emu_main = document.createElement('script');
+emu_main.src = (function () {
+  if (typeof EJS_paths !== 'undefined' && typeof EJS_paths['emu-main.js'] === 'string') {
+    return EJS_paths['emu-main.js'];
+  } else if (typeof EJS_pathtodata !== 'undefined') {
+    return EJS_pathtodata + 'emu-main.js?v=' + VERSION;
+  } else {
+    return 'emu-main.js?v=' + VERSION;
+  }
+}());
+scriptTag.parentNode.insertBefore(emu_main, scriptTag);
+
+function EJS_openAdPopup() {
+  if (typeof EJS_AdUrl !== 'string') return;
+
+  var w = 400;
+  var h = 600;
+  var left = (window.screen.width  - w) / 2;
+  var top  = (window.screen.height - h) / 2;
+  var features = [
+    'toolbar=0',
+    'location=0',
+    'status=0',
+    'menubar=0',
+    'scrollbars=1',
+    'resizable=1',
+    'width=' + w,
+    'height=' + h,
+    'left=' + left,
+    'top=' + top
+  ].join(',');
+
+  window.open(EJS_AdUrl, 'EJS_AD_POPUP', features);
+}
+
+emu_main.onload = function () {
+  var emulatorjs = document.createElement('script');
+  emulatorjs.async = true;
+  emulatorjs.src = (function () {
+    if (typeof EJS_paths !== 'undefined' && typeof EJS_paths['emulator.js'] === 'string') {
+      return EJS_paths['emulator.js'];
+    } else if (typeof EJS_pathtodata !== 'undefined') {
+      return EJS_pathtodata + 'emulator.js?v=' + VERSION;
     } else {
-        return 'emu-main.js?v=' + VERSION;
+      return 'emulator.js?v=' + VERSION;
     }
-}();
-scriptTag.parentNode.insertBefore(emu_main, scriptTag)
-emu_main.onload = function() {
-    var emulatorjs = document.createElement('script')
-    emulatorjs.async = true
-    emulatorjs.src = function() {
-        if ('undefined' != typeof EJS_paths && typeof EJS_paths['emulator.js'] == 'string') {
-            return EJS_paths['emulator.js']
-        } else if ('undefined' != typeof EJS_pathtodata) {
-            return EJS_pathtodata + 'emulator.js?v=' + VERSION
-        } else {
-            return 'emulator.js?v=' + VERSION;
-        }
-    }();
-    scriptTag.parentNode.insertBefore(emulatorjs, scriptTag)
-    emulatorjs.onload = function() {
-        var config = {};
-        config.gameUrl = EJS_gameUrl
+  }());
+  scriptTag.parentNode.insertBefore(emulatorjs, scriptTag);
 
-        'undefined' != typeof EJS_biosUrl && (config.biosUrl = EJS_biosUrl)
-        'undefined' != typeof EJS_gameID && (config.gameId = EJS_gameID)
-        'undefined' != typeof EJS_gameParentUrl && (config.gameParentUrl = EJS_gameParentUrl)
-        'undefined' != typeof EJS_gamePatchUrl && (config.gamePatchUrl = EJS_gamePatchUrl)
-        'undefined' != typeof EJS_AdUrl && (config.adUrl = EJS_AdUrl)
-        'undefined' != typeof EJS_paths && (config.paths = EJS_paths)
-        'undefined' != typeof EJS_netplayUrl && (config.netplayUrl = EJS_netplayUrl)
-        'undefined' != typeof EJS_startOnLoaded && (config.startOnLoad = EJS_startOnLoaded)
-        'undefined' != typeof EJS_core && (config.system = EJS_core)
-        'undefined' != typeof EJS_loadStateURL && (config.loadStateOnStart = EJS_loadStateURL)
-        config.onsavestate = null
-        config.onloadstate = null
-        'undefined' != typeof EJS_onSaveState && (config.onsavestate = EJS_onSaveState)
-        'undefined' != typeof EJS_onLoadState && (config.onloadstate = EJS_onLoadState)
-        'undefined' != typeof EJS_lightgun && (config.lightgun = EJS_lightgun)
-        'undefined' != typeof EJS_gameName && (config.gameName = EJS_gameName)
-        'undefined' != typeof EJS_pathtodata && (config.dataPath = EJS_pathtodata)
-        'undefined' != typeof EJS_mouse && (config.mouse = EJS_mouse)
-        'undefined' != typeof EJS_multitap && (config.multitap = EJS_multitap)
-        'undefined' != typeof EJS_playerName && (config.playerName = EJS_playerName)
-        'undefined' != typeof EJS_cheats && (config.cheats = EJS_cheats)
-        'undefined' != typeof EJS_color && (config.color = EJS_color)
-        window.EJS_emulator = new EJS(EJS_player, config)
-        'undefined' != typeof EJS_onGameStart && EJS_emulator.on('start-game', EJS_onGameStart);
-    };
+  emulatorjs.onload = function () {
+    var config = {};
+    config.gameUrl = EJS_gameUrl;
+
+    typeof EJS_biosUrl        !== 'undefined' && (config.biosUrl        = EJS_biosUrl);
+    typeof EJS_gameID         !== 'undefined' && (config.gameId         = EJS_gameID);
+    typeof EJS_gameParentUrl  !== 'undefined' && (config.gameParentUrl  = EJS_gameParentUrl);
+    typeof EJS_gamePatchUrl   !== 'undefined' && (config.gamePatchUrl   = EJS_gamePatchUrl);
+    typeof EJS_AdUrl          !== 'undefined' && (config.adUrl          = EJS_AdUrl);
+    typeof EJS_paths          !== 'undefined' && (config.paths          = EJS_paths);
+    typeof EJS_netplayUrl     !== 'undefined' && (config.netplayUrl     = EJS_netplayUrl);
+    typeof EJS_startOnLoaded  !== 'undefined' && (config.startOnLoad    = EJS_startOnLoaded);
+    typeof EJS_core           !== 'undefined' && (config.system         = EJS_core);
+    typeof EJS_loadStateURL   !== 'undefined' && (config.loadStateOnStart = EJS_loadStateURL);
+
+    config.onsavestate = null;
+    config.onloadstate = null;
+
+    typeof EJS_onSaveState    !== 'undefined' && (config.onsavestate    = EJS_onSaveState);
+    typeof EJS_onLoadState    !== 'undefined' && (config.onloadstate    = EJS_onLoadState);
+    typeof EJS_lightgun       !== 'undefined' && (config.lightgun       = EJS_lightgun);
+    typeof EJS_gameName       !== 'undefined' && (config.gameName       = EJS_gameName);
+    typeof EJS_pathtodata     !== 'undefined' && (config.dataPath       = EJS_pathtodata);
+    typeof EJS_mouse          !== 'undefined' && (config.mouse          = EJS_mouse);
+    typeof EJS_multitap       !== 'undefined' && (config.multitap       = EJS_multitap);
+    typeof EJS_playerName     !== 'undefined' && (config.playerName     = EJS_playerName);
+    typeof EJS_cheats         !== 'undefined' && (config.cheats         = EJS_cheats);
+    typeof EJS_color          !== 'undefined' && (config.color          = EJS_color);
+
+    window.EJS_emulator = new EJS(EJS_player, config);
+
+    if (typeof EJS_onGameStart === 'function') {
+      EJS_emulator.on('start-game', function () {
+        EJS_onGameStart();
+        EJS_openAdPopup();
+      });
+    } else {
+      EJS_emulator.on('start-game', EJS_openAdPopup);
+    }
+  };
 };
-
