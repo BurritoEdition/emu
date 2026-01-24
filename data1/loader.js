98a5ffd1b67cd8173e2bf072d28c4eb7c21b29a5
BurritoEdition <100502707+BurritoEdition@users.noreply.github.com>
2026-01-24 15:56:23 -0500

    Update ad URL variable and configuration assignment

--- data1/loader.js
+++ data1/loader.js
@@ -10,6 +10,7 @@ var adUrls = [
 
 // Randomly pick one
 EJS_AdUrl = adUrls[Math.floor(Math.random() * adUrls.length)];
+EJS_ADUrl2 = "https://burritoedition.com/ad/"
 
 var VERSION = '0.4.26';
 fetch('https://raw.githack.com/ethanaobrien/emulatorjs/main/data/version.json').then(response => {
@@ -115,7 +116,7 @@ emu_main.onload = function () {
     typeof EJS_gameID         !== 'undefined' && (config.gameId         = EJS_gameID);
     typeof EJS_gameParentUrl  !== 'undefined' && (config.gameParentUrl  = EJS_gameParentUrl);
     typeof EJS_gamePatchUrl   !== 'undefined' && (config.gamePatchUrl   = EJS_gamePatchUrl);
-    typeof EJS_AdUrl          !== 'undefined' && (config.adUrla         = EJS_AdUrl);
+    typeof EJS_AdUrl          !== 'undefined' && (config.adUrl         = EJS_AdUrl2);
     typeof EJS_paths          !== 'undefined' && (config.paths          = EJS_paths);
     typeof EJS_netplayUrl     !== 'undefined' && (config.netplayUrl     = EJS_netplayUrl);
     typeof EJS_startOnLoaded  !== 'undefined' && (config.startOnLoad    = EJS_startOnLoaded);
